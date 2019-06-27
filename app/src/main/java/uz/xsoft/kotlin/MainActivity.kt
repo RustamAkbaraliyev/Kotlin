a6405935d2d0e995525cf2edc765407d41695c82
Rustam Akbaraliyev <rustam.akbaraliyev@gmail.com>
2019-06-27 18:49:46 +0500

    commit

---
+++ app/src/main/java/uz/xsoft/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package uz.xsoft.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
