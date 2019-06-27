9bcf64a2266cf0e4ca787bb3272719da18927518
laptop <rustam.akbaraliyev@gmail.com>
2019-06-27 19:20:29 +0500

    changes

--- app/src/main/java/uz/xsoft/kotlin/MainActivity.kt
+++ app/src/main/java/uz/xsoft/kotlin/MainActivity.kt
@@ -2,12 +2,15 @@ package uz.xsoft.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //this is my commit
+        dev.text = "Dev instensive"
     }
 }
 
